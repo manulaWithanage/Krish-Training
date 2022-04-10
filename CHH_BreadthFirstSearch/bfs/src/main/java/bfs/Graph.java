f0657a49ff2b5f747ba746a9093ed9e631b762d1
manulaWithanage <57287133+manulaWithanage@users.noreply.github.com>
2022-04-10 21:27:01 +0530

    Updated

--- CHH_BreadthFirstSearch/bfs/src/main/java/bfs/Graph.java
+++ CHH_BreadthFirstSearch/bfs/src/main/java/bfs/Graph.java
@@ -1,3 +1,5 @@
+package bfs;
+
 import java.util.*;
 
 public class Graph {
@@ -44,10 +46,10 @@ public class Graph {
 
     g.addEdge(0, 1);
     g.addEdge(0, 2);
-    g.addEdge(1, 2);
-    g.addEdge(2, 0);
-    g.addEdge(2, 3);
-    g.addEdge(3, 3);
+    g.addEdge(1, 3);
+    g.addEdge(2, 4);
+    g.addEdge(2, 6);
+    g.addEdge(4, 5);
 
     System.out.println("Following is Breadth First Traversal " + "(starting from vertex 2)");
 
--- CHH_DepthFirstSearch/dfs/src/main/java/dfs/App.java
+++ CHH_DepthFirstSearch/dfs/src/main/java/dfs/App.java
@@ -1,3 +1,5 @@
+package dfs;
+
 import java.util.*;
 
 class Graph {
@@ -34,8 +36,10 @@ class Graph {
 
     g.addEdge(0, 1);
     g.addEdge(0, 2);
-    g.addEdge(1, 2);
-    g.addEdge(2, 3);
+    g.addEdge(1, 3);
+    g.addEdge(2, 4);
+    g.addEdge(2, 6);
+    g.addEdge(4, 5);
 
     System.out.println("Following is Depth First Traversal");
 
